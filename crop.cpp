bb7bf3f5290c0ba885a76a83949cd78cbfbfb791
heruv <alexfake678@gmail.com>
2025-03-03 10:36:49 +0300

    feat: add flip function

--- crop.cpp
+++ crop.cpp
@@ -4,6 +4,12 @@ Crop::Crop(string path_to_img, int domain_size, int rang_size)
 {
     input_img_ = imread(path_to_img, IMREAD_GRAYSCALE);
 
+    if (input_img_.empty())
+    {
+        cout << "Unable to read image" << std::endl;
+        abort();
+    }
+
     domain_bloc_size_ = domain_size;
     rang_bloc_size_ = rang_size;
 
@@ -15,8 +21,7 @@ Crop::Crop(string path_to_img, int domain_size, int rang_size)
     output_img_.create(new_rows, new_cols, CV_8UC1);
 
     split();
-    //merge();
-    rotate();
+    merge();
 
     imshow("merge", output_img_);
     waitKey(0);
@@ -68,6 +73,21 @@ void Crop::rotate()
         {
             Mat m = getRotationMatrix2D(Point((rois_.at(n).cols -1)/ 2.0f, (rois_.at(n).rows -1)/ 2.0f), 90.0, 1.0);
             warpAffine(rois_.at(n), rois_.at(n), m, rois_.at(0).size());
+
+            rois_.at(n).copyTo(output_img_(Rect(j, i, rang_bloc_size_, rang_bloc_size_)));
+            n++;
+        }
+    }
+}
+
+void Crop::flip()
+{
+    int n = 0;
+    for (int i = 0; i < output_img_.rows; i += rang_bloc_size_)
+    {
+        for (int j = 0; j < output_img_.cols; j += rang_bloc_size_)
+        {
+            cv::flip(rois_.at(n), rois_.at(n), 1);
 
             rois_.at(n).copyTo(output_img_(Rect(j, i, rang_bloc_size_, rang_bloc_size_)));
             n++;
--- crop.h
+++ crop.h
@@ -30,6 +30,7 @@ private:
 
 
 	void rotate();
+	void flip();
 };
 
 
